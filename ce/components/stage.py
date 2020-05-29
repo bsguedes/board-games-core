c5b458af0ed7c40decfc5838a812f6bcedc5fd37
Bruno Guedes <bsguedes@inf.ufrgs.br>
2020-05-29 19:21:06 -0300

    auto reroll

--- ce/components/stage.py
+++ ce/components/stage.py
@@ -1,10 +1,12 @@
 from ce.components.die import Die
 from typing import List
 
+DICE_COUNT = 5
+
 
 class Stage:
     def __init__(self):
-        self.dice: List[Die] = [Die() for _ in range(5)]
+        self.dice: List[Die] = [Die() for _ in range(DICE_COUNT)]
         self.on_stage: List[Die] = self.dice
         self.off_stage: List[Die] = []
 
@@ -12,9 +14,11 @@ class Stage:
         if die in self.on_stage:
             self.on_stage.remove(die)
             self.off_stage.append(die)
+        if len(self.on_stage) == DICE_COUNT:
+            self.reroll_all_dice()
         return die.upper_face
 
-    def reroll_all(self) -> None:
+    def reroll_all_dice(self) -> None:
         self.on_stage = self.dice
         for die in self.dice:
             die.roll()
@@ -26,7 +30,10 @@ class Stage:
     def upper_faces_off_stage(self) -> List[str]:
         return [die.upper_face for die in self.dice]
 
-    def roll_off(self, face) -> int:
+    def off_stage_count(self) -> int:
+        return len(self.off_stage)
+
+    def roll_dice_off_stage(self, face) -> int:
         for die in self.off_stage:
             die.roll()
         return len([die for die in self.off_stage if die.upper_face == face])
